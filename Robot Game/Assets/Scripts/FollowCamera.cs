2eca0af50a5ad82e605ddfe57307ce052ade4959
JulianBritton13 <46837272+JulianBritton13@users.noreply.github.com>
2020-06-03 17:53:45 +0200

    Added camera and player ship playholder.

---
+++ Robot Game/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform playerShip;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = playerShip.position + offset;
+    }
+}
---
+++ Robot Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float forwardSpeed = 100f;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+       // player.AddForce(0, 0, forwardSpeed * Time.deltaTime);
+        player.AddRelativeForce(0, 0, forwardSpeed * Time.deltaTime);
+    }
+}
